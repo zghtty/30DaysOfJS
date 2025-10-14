2c233e73e65c7531509081693dd2ca99ce6c482d
zghtty <zgh040116@gmail.com>
2025-10-14 16:40:12 +0800

    initial commit: 初始化项目

---
+++ 1/helloworld.js
@@ -0,0 +1,19 @@
+console.log('Hello, World!');
+
+// Declaring different variables of different data types
+let firstName = 'Asabeneh' // first name of a person
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age = 100 // age in years
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age, isMarried)
+
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in °C
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI)
+
+let name = 'Asabeneh', job = 'teacher', live = 'Finland'
+console.log(name, job, live)
